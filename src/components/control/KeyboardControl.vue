5c13b0f4bdade554ccf0b173200d9b127ccf8ea0
Konrad Brzózka <hadenir@gmail.com>
2021-12-06 20:02:20 +0000

    Resolve "Paczka ROS uruchamiająca serwery dla interfejsu"

---
+++ src/components/control/KeyboardControl.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label>
+      Keyboard control component.
+    </label>
+  </div>
+</template>
+<script>
+    export default {
+      name: "KeyboardControl",
+      props: {
+          'ws_address': String,
+          'ros': Object,
+      },
+      mounted() {
+        console.log(this.ws_address);
+        console.log(this.ros);
+      },
+    };
+</script>
--- src/views/Control.vue
+++ src/views/Control.vue
@@ -1,40 +1,25 @@
 <template>
     <div class="control">
         <h1>This is the control page.</h1>
-        <div>
-            <label>
-                Connected to {{this.ws_address}}
-            </label>
-            <label>
-                Gamepad API supported: {{this.gamepadAPIallowed}}
-            </label>
-            <label>
-                Gamepads: {{JSON.stringify(this.gamepads)}}
-            </label>
-            <label>
-            </label>
-        </div>
+        <keyboard-control
+          :ws_address="this.ws_address"
+          :ros="this.ros"/>
     </div>
 </template>
 <script>
+    import KeyboardControl from '@/components/control/KeyboardControl.vue';
     export default {
         name: "Control",
         
+        components: {
+          KeyboardControl,
+        },
         props: {
             'ws_address': String,
             'ros': Object,
         },
 
-        data ()  {
-          return {
-            gamepadAPIallowed: (navigator.getGamepads != null),
-            gamepads: (navigator.getGamepads()),
-          }
-        },
-
         mounted() {
-          console.log(navigator.getGamepads);
-          
           // If ROS got disconnected, return to home
           if (this.ros==null) {
             this.$router.push({
