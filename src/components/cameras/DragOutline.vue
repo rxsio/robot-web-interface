c290229d8e430b363934658c185ac1f683de0f52
Adam Jeliński <adas.jelinski@gmail.com>
2021-12-28 22:16:57 +0100

    Add component for highlighting dragging targets

---
+++ src/components/cameras/DragOutline.vue
@@ -0,0 +1,37 @@
+<template>
+    <div :style="style" class="outline"></div>
+</template>
+<script>
+export default {
+    name: 'DragOutline',
+
+    props: {
+        positionX: Number,
+        positionY: Number,
+        width: Number,
+        height: Number,
+    },
+
+    computed: {
+        style() {
+            return {
+                top: this.positionY + 'px',
+                left: this.positionX + 'px',
+                height: this.height + 'px',
+                width: this.width + 'px',
+            }
+        },
+    },
+}
+</script>
+<style scoped>
+.outline {
+    position: absolute;
+
+    border: 5px solid rgba(0, 200, 150, 1);
+    background-color: rgba(0, 200, 150, 0.2);
+    border-radius: 5px;
+
+    transition: width 0.5s, height 0.5s, top 0.5s, left 0.5s;
+}
+</style>
--- src/views/Cameras.vue
+++ src/views/Cameras.vue
@@ -1,68 +1,120 @@
 <template>
     <div class="cameras">
-        <div>
-        </div>
+        <DragOutline
+            :positionX="outlinePos.x"
+            :positionY="outlinePos.y"
+            :height="outlinePos.height"
+            :width="outlinePos.width"
+        />
+        <div></div>
         <div id="mjpeg" class="viewport"></div>
     </div>
 </template>
 <script>
-    export default {
-        name: "Cameras",
-        
-        props: {
-            'ws_address': String,
-            'ros': Object,
-        },
+import DragOutline from '@/components/cameras/DragOutline.vue'
+export default {
+    components: { DragOutline },
+    name: 'Cameras',
 
-        data () {
-          return {
-          port: 8082,
-          Ros: this.ros,
-          };
-        },
+    props: {
+        ws_address: String,
+        ros: Object,
+    },
 
-        computed: {
-          video_address() {
-          return this.ws_address.slice(5, -5);
-          },
-        },
+    data() {
+        return {
+            port: 8082,
+            Ros: this.ros,
+            animInterval: null,
+            animState: 0,
+            outlinePos: {
+                x: 100,
+                y: 100,
+                height: 100,
+                width: 100,
+            },
+        }
+    },
 
-        mounted() {
-          // If ROS got disconnected, return to home
-          if (this.ros==null) {
+    computed: {
+        video_address() {
+            return this.ws_address.slice(5, -5)
+        },
+    },
+
+    mounted() {
+        // If ROS got disconnected, return to home
+        if (this.ros == null) {
             this.$router.push({
-              name: "Home",
-            });
-          }
+                name: 'Home',
+            })
+        }
 
-          // Read vidoe server port
-          // Create a Param object for the port
-          var portParam = new window.ROSLIB.Param({
-            ros : this.Ros,
-            name :  '/web_video_server/port'
-          });
+        // Read vidoe server port
+        // Create a Param object for the port
+        var portParam = new window.ROSLIB.Param({
+            ros: this.Ros,
+            name: '/web_video_server/port',
+        })
 
-          // Get the number of the port
-          portParam.get(this.setPort);
-        },
-        methods: {
-          setPort(value) {
-            this.port = value;
+        // Get the number of the port
+        portParam.get(this.setPort)
+
+        // testing the outline animations
+        this.animInterval = setInterval(() => {
+            switch (this.animState) {
+                case 0:
+                    this.outlinePos = {
+                        x: 100,
+                        y: 100,
+                        height: 100,
+                        width: 100,
+                    }
+                    break
+                case 1:
+                    this.outlinePos = {
+                        x: 1000,
+                        y: 300,
+                        height: 50,
+                        width: 200,
+                    }
+                    break
+                case 2:
+                    this.outlinePos = {
+                        x: 0,
+                        y: 0,
+                        height: 1080,
+                        width: 1920,
+                    }
+                    break
+            }
+            this.animState++
+            this.animState %= 3
+        }, 500)
+    },
+    methods: {
+        setPort(value) {
+            this.port = value
             new window.MJPEGCANVAS.MultiStreamViewer({
-            divID : 'mjpeg',
-            host : this.video_address,
-            port : this.port,
-            width : 640,
-            height : 480,
-            topics : [ '/sirius/camera/image_raw', '/sirius/camera/image_raw',
-              '/sirius/camera/image_raw' ],
-            labels : [ 'View1', 'View2', 'View3' ]
-            });
-          },
+                divID: 'mjpeg',
+                host: this.video_address,
+                port: this.port,
+                width: 640,
+                height: 480,
+                topics: [
+                    '/sirius/camera/image_raw',
+                    '/sirius/camera/image_raw',
+                    '/sirius/camera/image_raw',
+                ],
+                labels: ['View1', 'View2', 'View3'],
+            })
         },
-        beforeDestroy() {
-          // Reloading page when going back
-          window.location.reload();
-        },
-    };
+    },
+    beforeDestroy() {
+        // Reloading page when going back
+        window.location.reload()
+
+        clearInterval(this.animInterval)
+    },
+}
 </script>
