047ee18dfb75ba0b18775b69ffdfac89489f6869
Adam Jeliński <adas.jelinski@gmail.com>
2022-09-16 17:33:10 +0200

    Loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import LoadingScreen from '@/components/LoadingScreen.vue'
+</script>
 <template>
     <v-app>
+        <LoadingScreen />
         <RouterView />
     </v-app>
 </template>
