dbe0f6c7ab3fb039550894c14bf6f15cdec2922e
Mateusz533 <mateuszfrejlich533@gmail.com>
2023-03-18 23:13:11 +0100

    Some optimizations

--- src/components/ControlWindow/KeyboardControl.vue
+++ src/components/ControlWindow/KeyboardControl.vue
@@ -8,19 +8,25 @@ export default {
     data() {
         return {
             topic: null,
-            max_linear_speed: 1,
-            max_angular_speed: 1.57,
-            linear_speed_percentage: 25,
-            angular_speed_percentage: 25,
             timer: null,
             message_rate: 100, // [ms]
-            pressed_W: false,
-            pressed_A: false,
-            pressed_S: false,
-            pressed_D: false,
-            elements: ['linear_speed', 'angular_speed'],
+            max_linear_speed: 1,
+            max_angular_speed: 1.57,
             focus_index: 0,
             is_focused: false,
+            pressed: { W: false, A: false, S: false, D: false },
+            elements: [
+                {
+                    id: 'linear_speed',
+                    text: 'Linear velocity',
+                    speed_percentage: 25,
+                },
+                {
+                    id: 'angular_speed',
+                    text: 'Angular velocity',
+                    speed_percentage: 25,
+                },
+            ],
             key_down_callback: (event) => {
                 this.keyListener(event, true)
             },
@@ -32,13 +38,13 @@ export default {
     methods: {
         startPublishing() {
             this.timer = setInterval(() => {
-                var message = new Message({
+                let message = new Message({
                     linear: {
                         x:
-                            (this.pressed_W - this.pressed_S) *
+                            (this.pressed.W - this.pressed.S) *
                             this.max_linear_speed *
                             0.01 *
-                            this.linear_speed_percentage,
+                            this.elements[0].speed_percentage,
                         y: 0,
                         z: 0,
                     },
@@ -46,63 +52,39 @@ export default {
                         x: 0,
                         y: 0,
                         z:
-                            (this.pressed_A - this.pressed_D) *
+                            (this.pressed.A - this.pressed.D) *
                             this.max_angular_speed *
                             0.01 *
-                            this.angular_speed_percentage,
+                            this.elements[1].speed_percentage,
                     },
                 })
+                // console.log(message)
                 this.topic.publish(message)
             }, this.message_rate)
         },
         keyListener(e, isPressed) {
             if (!this.is_focused) return
-            var key = e.key.toUpperCase()
-            switch (key) {
-                case 'W':
-                    this.pressed_W = isPressed
-                    break
-                case 'S':
-                    this.pressed_S = isPressed
-                    break
-                case 'A':
-                    this.pressed_A = isPressed
-                    break
-                case 'D':
-                    this.pressed_D = isPressed
-                    break
-                case ' ':
-                    if (isPressed)
-                        this.focus_index =
-                            (this.focus_index + 1) % this.elements.length
-                    document
-                        .getElementById(this.elements[this.focus_index])
-                        .focus()
-                    break
-                default:
-                    break
+            let key = e.key.toUpperCase()
+
+            if (this.pressed[key] !== undefined) this.pressed[key] = isPressed
+
+            if (key === 'TAB' && isPressed) {
+                this.focus_index = (this.focus_index + 1) % this.elements.length
+                document
+                    .getElementById(this.elements[this.focus_index].id)
+                    .focus()
             }
-        },
-        sliderInputCallback() {
-            // this.$cookies.set(
-            //     'linear-speed-percentage',
-            //     this.linear_speed_percentage
-            // )
-            // this.$cookies.set(
-            //     'angular-speed-percentage',
-            //     this.angular_speed_percentage
-            // )
         },
         focus() {
             this.is_focused = true
-            document.getElementById(this.elements[this.focus_index]).focus()
+            document.getElementById(this.elements[this.focus_index].id).focus()
         },
         unfocus() {
             this.is_focused = false
-            this.pressed_W = false
-            this.pressed_A = false
-            this.pressed_S = false
-            this.pressed_D = false
+            this.pressed.W = false
+            this.pressed.A = false
+            this.pressed.S = false
+            this.pressed.D = false
         },
     },
     mounted() {
@@ -114,21 +96,9 @@ export default {
         window.addEventListener('keydown', this.key_down_callback)
         window.addEventListener('keyup', this.key_up_callback)
 
-        // Read previous percentage settings
-        // if (this.$cookies.isKey('linear-speed-percentage')) {
-        //     this.linear_speed_percentage = this.$cookies.get(
-        //         'linear-speed-percentage'
-        //     )
-        // }
-        // if (this.$cookies.isKey('angular-speed-percentage')) {
-        //     this.angular_speed_percentage = this.$cookies.get(
-        //         'angular-speed-percentage'
-        //     )
-        // }
-
         // Read maximum speed from ros params
-        var base = '/web_interface/control'
-        var maxLinearSpeedParam = new Param({
+        let base = '/web_interface/control'
+        let maxLinearSpeedParam = new Param({
             ros: this.ros,
             name: base + '/linear/x/max_velocity',
         })
@@ -137,7 +107,7 @@ export default {
                 this.max_linear_speed = value
             }
         })
-        var maxAngularSpeedParam = new Param({
+        let maxAngularSpeedParam = new Param({
             ros: this.ros,
             name: base + '/angular/z/max_velocity',
         })
@@ -149,7 +119,7 @@ export default {
         this.startPublishing()
 
         // Set focus on first slider
-        document.getElementById(this.elements[this.focus_index]).focus()
+        document.getElementById(this.elements[this.focus_index].id).focus()
     },
     beforeDestroy() {
         clearInterval(this.timer)
@@ -165,136 +135,49 @@ export default {
         @focusout="unfocus()"
         @focusin="focus()"
     >
-        <div class="slidecontainer">
+        <div
+            v-for="(element, i) in this.elements"
+            :key="element.name"
+            class="slidecontainer"
+        >
             <td class="col1">
                 <input
                     type="range"
                     min="1"
                     max="100"
                     class="slider"
-                    :class="{ focused: focus_index == 0 && is_focused }"
-                    id="linear_speed"
-                    v-model="linear_speed_percentage"
-                    v-on:input="sliderInputCallback"
-                    @click="focus_index = 0"
+                    :class="{ focused: focus_index == i && is_focused }"
+                    :id="element.id"
+                    v-model="element.speed_percentage"
+                    @click="focus_index = i"
                 />
             </td>
             <td class="col2">
-                <label class="sliderLabel">Linear velocity:</label>
+                <label class="sliderLabel">{{ element.text }}:</label>
             </td>
             <td class="col3">
                 <label class="sliderLabel">
-                    {{ this.linear_speed_percentage }}%
-                </label>
-            </td>
-        </div>
-        <div class="slidecontainer">
-            <td class="col1">
-                <input
-                    type="range"
-                    min="1"
-                    max="100"
-                    class="slider"
-                    :class="{ focused: focus_index == 1 && is_focused }"
-                    id="angular_speed"
-                    v-model="angular_speed_percentage"
-                    v-on:input="sliderInputCallback"
-                    @click="focus_index = 1"
-                />
-            </td>
-            <td class="col2">
-                <label class="sliderLabel">Angular velocity:</label>
-            </td>
-            <td class="col3">
-                <label class="sliderLabel">
-                    {{ this.angular_speed_percentage }}%
+                    {{ element.speed_percentage }}%
                 </label>
             </td>
         </div>
 
         <div class="keyboardBox">
             <p>
-                <button :class="{ pressed: pressed_W }">W</button>
+                <button :class="{ pressed: pressed.W }">W</button>
             </p>
             <p>
-                <button :class="{ pressed: pressed_A }">A</button>
-                <button :class="{ pressed: pressed_S }">S</button>
-                <button :class="{ pressed: pressed_D }">D</button>
+                <button :class="{ pressed: pressed.A }">A</button>
+                <button :class="{ pressed: pressed.S }">S</button>
+                <button :class="{ pressed: pressed.D }">D</button>
             </p>
         </div>
         <p>
-            Use 'Space' to switch between sliders or 'Arrow Keys' to change each
+            Use 'TAB' to switch between sliders or 'Arrow Keys' to change each
             speed value.
         </p>
     </div>
 </template>
 <style>
 @import './control-styles.css';
-
-div.keyboardControl {
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    margin: 10px 5px;
-    width: calc(100% - 5px);
-    height: calc(100% - 10px);
-}
-
-div.keyboardControl div.slidecontainer {
-    height: 40px;
-    width: 80%;
-    display: inline-table;
-    padding: 5px;
-}
-
-div.keyboardControl div.slidecontainer td.col1 {
-    width: auto;
-    text-align: right;
-    vertical-align: middle;
-}
-
-div.keyboardControl div.slidecontainer td.col2 {
-    width: 8.5em;
-    text-align: left;
-    vertical-align: middle;
-}
-
-div.keyboardControl div.slidecontainer td.col3 {
-    width: 3em;
-    text-align: right;
-    vertical-align: middle;
-}
-
-div.keyboardControl div.slidecontainer .slider {
-    width: 90%;
-}
-div.keyboardControl div.slidecontainer .slider.focused {
-    background: var(--control-secondary-light);
-    height: 12px;
-}
-
-div.keyboardBox {
-    padding: 10px;
-    text-align: center;
-    background: #eee;
-    margin: 15px;
-    display: inline-block;
-    align-items: center;
-    border-radius: 15px;
-}
-
-div.keyboardBox p {
-    margin: 0;
-}
-
-div.keyboardBox p button {
-    margin: 8px;
-    width: 80px;
-    height: 80px;
-    font-size: 40px;
-}
-
-div.keyboardBox p button.pressed {
-    background-color: var(--control-secondary-light);
-}
 </style>
--- src/components/ControlWindow/ManipKeyboardControl.vue
+++ src/components/ControlWindow/ManipKeyboardControl.vue
@@ -9,58 +9,54 @@ export default {
         return {
             topic: null,
             timer: null,
-            max_effort: 1.0,
             message_rate: 100, // [ms]
-            pressed_Q: false,
-            pressed_E: false,
-            pressed_W: false,
-            pressed_A: false,
-            pressed_S: false,
-            pressed_D: false,
+            max_effort: 1.0,
             focus_index: 0,
             is_focused: false,
+            pressed: {
+                Q: false,
+                E: false,
+                W: false,
+                A: false,
+                S: false,
+                D: false,
+            },
             elements: [
                 {
-                    text: 'Arm rotate effort',
+                    id: 0,
                     name: 'arm_rotate',
-                    cookie: 'arm-rotate',
+                    text: 'Arm rotate effort',
                     effort_percentage: 25,
-                    id: parseInt(1000000 * Math.random()),
                 },
                 {
-                    text: 'Arm lift effort',
+                    id: 1,
                     name: 'arm_lift',
-                    cookie: 'arm-lift',
+                    text: 'Arm lift effort',
                     effort_percentage: 25,
-                    id: parseInt(1000000 * Math.random()),
                 },
                 {
-                    text: 'Arm tilt effort',
+                    id: 2,
                     name: 'arm_tilt',
-                    cookie: 'arm-tilt',
+                    text: 'Arm tilt effort',
                     effort_percentage: 25,
-                    id: parseInt(1000000 * Math.random()),
                 },
                 {
-                    text: 'Claw rotate effort',
+                    id: 3,
                     name: 'claw_rotate',
-                    cookie: 'claw-rotate',
+                    text: 'Claw rotate effort',
                     effort_percentage: 25,
-                    id: parseInt(1000000 * Math.random()),
                 },
                 {
-                    text: 'Claw lift effort',
+                    id: 4,
                     name: 'claw_lift',
-                    cookie: 'claw-lift',
+                    text: 'Claw lift effort',
                     effort_percentage: 25,
-                    id: parseInt(1000000 * Math.random()),
                 },
                 {
-                    text: 'Claw clamp effort',
+                    id: 5,
                     name: 'claw_clamp',
-                    cookie: 'claw-clamp',
+                    text: 'Claw clamp effort',
                     effort_percentage: 25,
-                    id: parseInt(1000000 * Math.random()),
                 },
             ],
             key_down_callback: (event) => {
@@ -74,12 +70,12 @@ export default {
     methods: {
         startPublishing() {
             this.timer = setInterval(() => {
-                var current_time = new Date()
-                var names = []
+                let current_time = new Date()
+                let names = []
                 this.elements.forEach((element) => {
                     names.push(element.name)
                 })
-                var message = new Message({
+                let message = new Message({
                     header: {
                         stamp: {
                             secs: Math.floor(current_time.getTime() / 1000),
@@ -97,33 +93,33 @@ export default {
                 })
                 if (this.focus_index >= 0 && this.focus_index <= 2) {
                     message.effort[0] =
-                        (this.pressed_A - this.pressed_D) *
+                        (this.pressed.A - this.pressed.D) *
                         this.max_effort *
                         0.01 *
                         this.elements[0].effort_percentage
                     message.effort[1] =
-                        (this.pressed_W - this.pressed_S) *
+                        (this.pressed.W - this.pressed.S) *
                         this.max_effort *
                         0.01 *
                         this.elements[1].effort_percentage
                     message.effort[2] =
-                        (this.pressed_Q - this.pressed_E) *
+                        (this.pressed.Q - this.pressed.E) *
                         this.max_effort *
                         0.01 *
                         this.elements[2].effort_percentage
                 } else if (this.focus_index >= 3 && this.focus_index <= 5) {
                     message.effort[3] =
-                        (this.pressed_A - this.pressed_D) *
+                        (this.pressed.A - this.pressed.D) *
                         this.max_effort *
                         0.01 *
                         this.elements[3].effort_percentage
                     message.effort[4] =
-                        (this.pressed_W - this.pressed_S) *
+                        (this.pressed.W - this.pressed.S) *
                         this.max_effort *
                         0.01 *
                         this.elements[4].effort_percentage
                     message.effort[5] =
-                        (this.pressed_Q - this.pressed_E) *
+                        (this.pressed.Q - this.pressed.E) *
                         this.max_effort *
                         0.01 *
                         this.elements[5].effort_percentage
@@ -133,48 +129,16 @@ export default {
         },
         keyListener(e, isPressed) {
             if (!this.is_focused) return
-            var key = e.key.toUpperCase()
-            switch (key) {
-                case 'Q':
-                    this.pressed_Q = isPressed
-                    break
-                case 'E':
-                    this.pressed_E = isPressed
-                    break
-                case 'W':
-                    this.pressed_W = isPressed
-                    break
-                case 'S':
-                    this.pressed_S = isPressed
-                    break
-                case 'A':
-                    this.pressed_A = isPressed
-                    break
-                case 'D':
-                    this.pressed_D = isPressed
-                    break
-                case ' ':
-                    // console.log(e)
-                    // console.log(document.activeElement)
-                    // console.log(document.getSelection())
-                    if (isPressed)
-                        this.focus_index =
-                            (this.focus_index + 1) % this.elements.length
-                    document
-                        .getElementById(this.elements[this.focus_index].id)
-                        .focus()
-                    break
-                default:
-                    break
+            let key = e.key.toUpperCase()
+
+            if (this.pressed[key] !== undefined) this.pressed[key] = isPressed
+
+            if (key === 'TAB' && isPressed) {
+                this.focus_index = (this.focus_index + 1) % this.elements.length
+                document
+                    .getElementById(this.elements[this.focus_index].id)
+                    .focus()
             }
-        },
-        sliderInputCallback() {
-            // for (i in this.elements.length) {
-            //     this.$cookies.set(
-            //         this.elements[i - 1].cookie,
-            //         this.elements[i - 1].effort_percentage
-            //     )
-            // }
         },
         focus() {
             this.is_focused = true
@@ -182,12 +146,12 @@ export default {
         },
         unfocus() {
             this.is_focused = false
-            this.pressed_Q = false
-            this.pressed_E = false
-            this.pressed_W = false
-            this.pressed_A = false
-            this.pressed_S = false
-            this.pressed_D = false
+            this.pressed.Q = false
+            this.pressed.E = false
+            this.pressed.W = false
+            this.pressed.A = false
+            this.pressed.S = false
+            this.pressed.D = false
         },
     },
     mounted() {
@@ -199,20 +163,11 @@ export default {
         window.addEventListener('keydown', this.key_down_callback)
         window.addEventListener('keyup', this.key_up_callback)
 
-        // Read previous percentage settings
-        // for (i in this.elements_names.length) {
-        //     if (this.$cookies.isKey(this.elements[i - 1].cookie)) {
-        //         this.elements[i - 1].effort_percentage = this.$cookies.get(
-        //             this.elements[i - 1].cookie
-        //         )
-        //     }
-        // }
-
         // Read maximum effort from ros params
-        var base = '/web_interface/control'
-        var maxEffortParam = new Param({
+        let base = '/web_interface/control'
+        let maxEffortParam = new Param({
             ros: this.ros,
-            name: base + '/max_effort', // do spradzenia czy istnieje ???
+            name: base + '/max_effort',
         })
         maxEffortParam.get((value) => {
             if (value != null) {
@@ -223,6 +178,11 @@ export default {
 
         // Set focus on first slider
         document.getElementById(this.elements[this.focus_index].id).focus()
+
+        // Set unique IDs
+        const randId = String(parseInt(1000000 * Math.random()))
+        for (let i = 0; i < this.elements.length; ++i)
+            this.elements[i].id = randId + i
     },
     beforeDestroy() {
         clearInterval(this.timer)
@@ -234,7 +194,7 @@ export default {
 
 <template>
     <div
-        class="control manipKeyboardControl"
+        class="control keyboardControl manip"
         @click.left="focus()"
         @focusout="unfocus()"
         @focusin="focus()"
@@ -253,7 +213,6 @@ export default {
                     :class="{ focused: focus_index == i && is_focused }"
                     :id="element.id"
                     v-model="element.effort_percentage"
-                    v-on:input="sliderInputCallback"
                     @click="focus_index = i"
                 />
             </td>
@@ -268,19 +227,19 @@ export default {
         </div>
         <div class="keyboardBox">
             <p>
-                <button :class="{ pressed: pressed_Q }">Q</button>
-                <button :class="{ pressed: pressed_W }">W</button>
-                <button :class="{ pressed: pressed_E }">E</button>
+                <button :class="{ pressed: pressed.Q }">Q</button>
+                <button :class="{ pressed: pressed.W }">W</button>
+                <button :class="{ pressed: pressed.E }">E</button>
             </p>
             <p>
-                <button :class="{ pressed: pressed_A }">A</button>
-                <button :class="{ pressed: pressed_S }">S</button>
-                <button :class="{ pressed: pressed_D }">D</button>
+                <button :class="{ pressed: pressed.A }">A</button>
+                <button :class="{ pressed: pressed.S }">S</button>
+                <button :class="{ pressed: pressed.D }">D</button>
             </p>
         </div>
 
         <p>
-            Use 'Space' to switch between sliders or 'Arrow Keys' to change each
+            Use 'TAB' to switch between sliders or 'Arrow Keys' to change each
             effort value.
         </p>
         <p>
@@ -312,70 +271,17 @@ export default {
 
 <style>
 @import './control-styles.css';
-div.manipKeyboardControl {
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    margin: 10px 5px;
-    width: calc(100% - 5px);
-    /* min-height: calc(100% - 10px); */
+
+div.keyboardControl.manip {
     height: max-content;
-    /* flex-grow: 1; */
 }
-div.manipKeyboardControl div.slidecontainer {
-    height: 40px;
-    width: 80%;
-    display: inline-table;
-    padding: 5px;
-}
-div.manipKeyboardControl div.slidecontainer td.col1 {
-    width: auto;
-    text-align: right;
-    vertical-align: middle;
-}
-div.manipKeyboardControl div.slidecontainer td.col2 {
-    width: 8.5em;
-    text-align: left;
-    vertical-align: middle;
-}
-div.manipKeyboardControl div.slidecontainer td.col3 {
-    width: 3em;
-    text-align: right;
-    vertical-align: middle;
-}
-div.manipKeyboardControl div.slidecontainer .slider {
-    width: 90%;
-}
-div.manipKeyboardControl div.slidecontainer .slider.focused {
-    background: var(--control-secondary-light);
-    height: 12px;
-}
-div.keyboardBox {
-    padding: 10px;
-    text-align: center;
-    background: #eee;
-    margin: 15px;
-    display: inline-block;
-    align-items: center;
-    border-radius: 15px;
-}
-div.keyboardBox p {
-    margin: 0;
-}
-div.keyboardBox p button {
-    margin: 8px;
-    width: 80px;
-    height: 80px;
-    font-size: 40px;
-}
-div.keyboardBox p button.pressed {
-    background-color: var(--control-secondary-light);
-}
-div.manipKeyboardControl div.description {
+
+div.keyboardControl.manip div.description {
     width: 22em;
     display: inline-block;
 }
-div.manipKeyboardControl div.description li {
+
+div.keyboardControl.manip div.description li {
     margin-left: 2em;
     padding: 0.25em;
     text-align: left;
