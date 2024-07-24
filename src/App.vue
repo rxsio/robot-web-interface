f24972ca396681ae207c1093d7093d07d9088e73
Patryk Filip Gryz <pfgryz@gmail.com>
2024-07-24 21:01:18 +0200

    Remove EncryptionErrorScreen.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import { RouterView } from 'vue-router'
 import {
     useJoystickStore,
@@ -8,46 +8,17 @@ import {
     useSteeringStore,
 } from '@/stores'
 import LoadingScreen from '@/components/LoadingScreen.vue'
-import EncryptionErrorScreen from '@/components/EncryptionErrorScreen.vue'
 
 const rosStore = useRosStore()
 const gstreamerStore = useGstreamerStore()
 const joystickStore = useJoystickStore()
 const steeringStore = useSteeringStore()
 
-const isReady = ref(null)
-
-const networkTest = async () => {
-    const protocol = window.location.protocol
-    const address = window.location.host
-    const href = window.location.href
-
-    if (protocol === 'https:' || window.webpackHotUpdaterobot_web_interface) {
-        isReady.value = true
-        return true
-    }
-
-    return await fetch(`https://${address}/networkTest`, { method: 'POST' })
-        .then(() => {
-            window.location.replace(`https:${href.substring(protocol.length)}`)
-            return true
-        })
-        .catch(() => {
-            isReady.value = false
-            return false
-        })
-}
-
 onMounted(() => {
-    networkTest().then((passed) => {
-        if (passed) {
-            gstreamerStore.connect()
-            //gstreamerStore
-            rosStore.connect()
-            joystickStore.start()
-            steeringStore.start()
-        }
-    })
+    gstreamerStore.connect()
+    rosStore.connect()
+    joystickStore.start()
+    steeringStore.start()
 })
 
 onUnmounted(() => {
@@ -57,11 +28,7 @@ onUnmounted(() => {
 </script>
 <template>
     <v-app>
-        <LoadingScreen v-if="isReady" />
-        <RouterView v-if="isReady" />
-        <EncryptionErrorScreen
-            v-if="!isReady"
-            :showError="isReady === false"
-        />
+        <LoadingScreen />
+        <RouterView />
     </v-app>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import Vue from 'vue'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+
+import App from '@/App.vue'
 
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
-
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import '@/styles/overrides.scss'
 
 Vue.use(PiniaVuePlugin)
 const pinia = createPinia()
-
-import App from '@/App.vue'
-import '@/styles/overrides.scss'
-
 Vue.config.productionTip = false
 
 const app = new Vue({
     router,
     vuetify,
     pinia,
-    render: (h) => h(App),
+    render: (h) => {
+        return h(App)
+    },
 })
 app.$mount('#app')
