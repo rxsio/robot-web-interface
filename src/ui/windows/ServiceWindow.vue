bbe5430bc6f61085625c79ebcf08afe12e471293
Patryk Filip Gryz <pfgryz@gmail.com>
2024-08-27 17:29:50 +0200

    Create stub windows for Service and Topic window. Fix displaying windows. Add getting ros topic and services

---
+++ src/ui/windows/ServiceWindow.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['extraConfig'])
+</script>
+
+<template>
+    <div>
+        SERVICE WINDOW: {{ props.extraConfig.service }}
+        <div
+            v-if="!props.extraConfig.service"
+            color="error"
+        >
+            Service not selected
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
